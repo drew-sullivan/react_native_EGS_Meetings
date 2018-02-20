62f417b94d9171b8642c5113386ba6e7a4f46922
Drew Sullivan <drew.sullivan.dma@gmail.com>
2018-02-19 22:15:59 -0500

    Trying to get first and last question button showing to work. WIP.

--- appButton.js
+++ appButton.js
@@ -3,36 +3,26 @@ import { Alert, View, StyleSheet, TouchableOpacity, Text } from "react-native";
 import { QUESTIONS_AND_ANSWERS } from "./shared/questionsAndAnswers";
 
 export class AppButton extends Component {
-  _onPressButton(btnTitle) {
+  _onPressButton(btnTitle, currentIndex) {
     if (btnTitle === "Yes") {
       this.props.incrementCurrentIndex();
-      this.props.isLastQuestion();
-    }
+    };
     if (btnTitle === "Previous") {
       this.props.decrementCurrentIndex();
-      this.props.isFirstQuestion();
-    } else {
-      return;
-    }
-  }
+    };
+  };
   render() {
-    const title = this.props.title;
-    const isYesNoButton = title !== "Previous";
-    const yesNoButtonStyles = [styles.button, styles.yesNoButton];
-    const prevButtonStyles = [styles.button, styles.prevButton];
     const isFirstQuestion = this.props.currentIndex === 0;
     const isLastQuestion = this.props.currentIndex === QUESTIONS_AND_ANSWERS.length - 1;
-    if (isFirstQuestion) {
-      return (
-        <TouchableOpacity>
-          <Text style={{display: 'none'}}>{title}</Text>
-        </TouchableOpacity>
-      );
-    }
+
+    const isYesNoButton = this.props.title !== "Previous";
+    const yesNoButtonStyles = [styles.button, styles.yesNoButton];
+    const prevButtonStyles = [styles.button, styles.prevButton];
+
     return (
       <TouchableOpacity
-        onPress={(e) => this._onPressButton(title)}>
-        <Text style={isYesNoButton ? yesNoButtonStyles : prevButtonStyles}>{title}</Text>
+        onPress={(e) => this._onPressButton(this.props.title, this.props.currentIndex)}>
+        <Text style={isYesNoButton ? yesNoButtonStyles : prevButtonStyles}>{this.props.title}</Text>
       </TouchableOpacity>
     );
   }
@@ -50,5 +40,8 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 10,
     fontSize: 20,
+  },
+  doNotDisplay: {
+    display: 'none'
   }
 });
